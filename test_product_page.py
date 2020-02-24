d383be2df12b97ee2b2f86f6dac6b1c2d771c3ad
LD\sbalzitova <sbalzitova@yandex-team.ru>
2020-02-24 15:05:23 +0300

    Marked tests for review

--- test_product_page.py
+++ test_product_page.py
@@ -19,6 +19,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_guest_not_see_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
@@ -34,6 +35,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_guest_not_see_success_message()
 
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -75,12 +77,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
